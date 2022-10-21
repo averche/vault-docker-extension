50289fe928dc1311e3d7aa45612f1cbd858402b4
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-10-21 01:02:13 -0400

    go files

---
+++ vm/go.mod
@@ -0,0 +1,32 @@
+module vault-docker-extension2
+
+go 1.19
+
+require (
+	github.com/hashicorp/vault-client-go v0.0.0-20221017195216-cf06ad42b983
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v0.16.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/exp v0.0.0-20220921164117-439092de6870 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+)
---
+++ vm/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/hashicorp/vault-client-go"
+
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+)
+
+var client *vault.Client
+
+func main() {
+	var socketPath string
+	flag.StringVar(&socketPath, "socket", "/run/guest/volumes-service.sock", "Unix domain socket to listen on")
+	flag.Parse()
+
+	os.RemoveAll(socketPath)
+
+	c, err := vault.NewClient(vault.Configuration{
+		BaseAddress: "http://vault:8201",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	client = c
+	client.SetToken("root")
+
+	logrus.New().Infof("Starting listening on %s\n", socketPath)
+	router := echo.New()
+	router.HideBanner = true
+
+	startURL := ""
+
+	ln, err := listen(socketPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	router.Listener = ln
+
+	router.GET("/hello", hello)
+	router.GET("/get-secrets", getSecrets)
+	router.GET("/get-secret", getSecret)
+	router.POST("/write-secret", writeSecret)
+
+	log.Fatal(router.Start(startURL))
+}
+
+func listen(path string) (net.Listener, error) {
+	return net.Listen("unix", path)
+}
+
+func hello(ctx echo.Context) error {
+	log.Println("asd")
+
+	resp, err := client.WithToken("root").System.GetSysMounts(ctx.Request().Context())
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, resp.Data)
+}
+
+func getSecrets(ctx echo.Context) error {
+	/* */ log.Println("getSecrets: started")
+	defer log.Println("getSecrets: done")
+
+	resp, err := client.List(ctx.Request().Context(), "/secret/metadata")
+	if err != nil {
+		return fmt.Errorf("error sending request to vault :%w", err)
+	}
+
+	keys, ok1 := resp.Data["keys"]
+	if !ok1 {
+		return fmt.Errorf("no secrets found")
+	}
+
+	return ctx.JSON(http.StatusOK, keys)
+}
+
+func getSecret(ctx echo.Context) error {
+	/* */ log.Println("getSecret:", ctx.QueryParam("key"), "started")
+	defer log.Println("getSecret:", ctx.QueryParam("key"), "done")
+
+	key := ctx.QueryParam("key")
+
+	resp, err := client.Read(ctx.Request().Context(), fmt.Sprintf("/secret/data/%s", key))
+	if err != nil {
+		return fmt.Errorf("error sending request to vault :%w", err)
+	}
+
+	return ctx.JSON(http.StatusOK, resp.Data["data"])
+}
+
+func writeSecret(ctx echo.Context) error {
+	/* */ log.Println("writeSecret:", ctx.QueryParam("key"), "started")
+	defer log.Println("writeSecret:", ctx.QueryParam("key"), "done")
+
+	defer ctx.Request().Body.Close()
+
+	key := ctx.QueryParam("key")
+
+	b, err := io.ReadAll(ctx.Request().Body)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.WriteFromBytes(
+		ctx.Request().Context(),
+		fmt.Sprintf("/secret/data/%s", key),
+		[]byte(fmt.Sprintf(`{"data": %s}`, string(b))),
+	)
+	if err != nil {
+		return fmt.Errorf("error sending request to vault :%w", err)
+	}
+
+	return ctx.JSON(http.StatusOK, resp.Data["data"])
+}
